40d890dc7cea2adf59dcc51f748be4c826f7f22a
Nayan Chowdhury <nayan.1aacl@gmail.com>
2025-04-20 00:12:04 +0600

    BIG O Rule 01

---
+++ Big O/index.js
@@ -0,0 +1,49 @@
+const nemo = ["nemo"];
+const everyOne = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel"];
+const largeArray = new Array(100000).fill("nemo");
+
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    console.log("running");
+    if (array[i] === "nemo") {
+      console.log("Found NEMO!");
+      break;
+    }
+  }
+  let t1 = performance.now();
+  return t1 - t0;
+}
+
+function findNemoForEach(array) {
+  let t0 = performance.now();
+  array.forEach((fish) => {
+    if (fish === "nemo") {
+      console.log("Found NEMO!");
+    }
+  });
+  let t1 = performance.now();
+  return t1 - t0;
+}
+
+const everyOneTime = findNemo(everyOne);
+console.log(`EveryOne found in ${everyOneTime} milliseconds`);
+// const nemoTime = findNemo(nemo);
+// console.log(`Nemo found in ${nemoTime} milliseconds`);
+// const largeArrayTime = findNemo(largeArray);
+// console.log(`Large Array found in ${largeArrayTime} milliseconds`);
+
+// const everyOneForEachTime = findNemoForEach(everyOne);
+// console.log(`EveryOne found in ${everyOneForEachTime} milliseconds`);
+// const nemoForEachTime = findNemoForEach(nemo);
+// console.log(`Nemo found in ${nemoForEachTime} milliseconds`);
+// const largeArrayForEachTime = findNemoForEach(largeArray);
+// console.log(`Large Array found in ${largeArrayForEachTime} milliseconds`);
+
+// function test() {
+//   console.log(a);
+//   if (true) {
+//     var a = 1;
+//   }
+// }
+// test();
